89796fd9f6b1bd66f223c84474ac4829b1aa0fac
Anushree-cloud <85478729+Anushree-cloud@users.noreply.github.com>
2021-07-16 18:40:36 +0530

    Delete buttons.js

--- buttons.js
+++
@@ -1,8 +0,0 @@
-const buttonHi = document.getElementById('btn1')
-const buttonHello = document.getElementById('btn2')
-const buttonHelloError = document.getElementById('btn3')
-
-buttonHi.addEventListener('click', ()=>{ toast('Hi') })
-buttonHello.addEventListener('click', ()=>{ toast({ message: 'hello', delay: 5000, type: 'success' }) })
-buttonHelloError.addEventListener('click', ()=>{ toast({ message: 'hello', delay: 5000, type: 'error' }) })
-
